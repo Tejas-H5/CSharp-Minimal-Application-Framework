a8a06d249484c32002bda18bb131bc9618f1cbae
Tejas Hegde <45031546+El-Tejaso@users.noreply.github.com>
2021-04-06 16:05:42 +0930

    Added some matrix creation convenience functions

--- RenderingEngine/Util/MathUtil.cs
+++ RenderingEngine/Util/MathUtil.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenTK.Mathematics;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
@@ -79,6 +80,31 @@ namespace RenderingEngine.Util
             float yR =  y *cos + x * sin;
 
             return new PointF(center.X + xR, center.Y + yR);
+        }
+
+        public static Matrix4 Translation(float x, float y, float z = 0)
+        {
+            return Matrix4.Transpose(Matrix4.CreateTranslation(x, y, z));
+        }
+
+        public static Matrix4 Rotation2D(float zAngles)
+        {
+            return Matrix4.CreateRotationZ(zAngles);
+        }
+
+        public static Matrix4 Rotation(float x, float y, float z)
+        {
+            return Matrix4.CreateRotationX(x) * Matrix4.CreateRotationY(y) * Matrix4.CreateRotationY(z);
+        }
+
+        public static Matrix4 Scale(float sf)
+        {
+            return Matrix4.CreateScale(sf);
+        }
+
+        public static Matrix4 Scale(float x, float y, float z)
+        {
+            return Matrix4.CreateScale(x, y, z);
         }
     }
 }

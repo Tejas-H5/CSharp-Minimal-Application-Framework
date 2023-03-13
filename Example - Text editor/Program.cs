5594da8541f80b507fa18a848685a522195dc6bc
Tejas Hegde <45031546+El-Tejaso@users.noreply.github.com>
2023-03-14 07:10:20 +1030

    Started example project

---
+++ Example - Text editor/Program.cs
@@ -0,0 +1,31 @@
+﻿using MinimalAF;
+
+namespace TextEditor {
+
+    static class AppColors {
+        public static Color BG => Color.White;
+        public static Color FG => Color.Black;
+    }
+
+
+    class TextEditor : IRenderable {
+        public void Render(FrameworkContext ctx) {
+            ctx.SetFont("Source code pro", 24);
+            ctx.SetDrawColor(AppColors.FG);
+            ctx.DrawText("Test", 10, ctx.VH - 10, HAlign.Left, VAlign.Top);
+        }
+    }
+
+
+    class Program {
+        static void Main(string[] args) {
+            new ProgramWindow((FrameworkContext ctx) => {
+                ctx.SetClearColor(AppColors.BG);
+                ctx.Window.Title = "Text editor";
+                ctx.Window.SetWindowState(WindowState.Maximized);
+
+                return new TextEditor();
+            }).Run();
+        }
+    }
+}

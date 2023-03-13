2b9a20925dbed7a41874993faf8a5b130cca2acd
Tejas Hegde <45031546+El-Tejaso@users.noreply.github.com>
2023-03-13 21:36:16 +1030

    Removed UI tests. UI is too opinionated and bad

--- Tests - UI/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using MinimalAF;
-
-namespace UIVisualTests {
-    class Program {
-        static void Main(string[] args) {
-            new ApplicationWindow().Run(new VisualTestRunner(typeof(UIContentSizeFittingTest)));
-        }
-    }
-}
